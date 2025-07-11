f52cd62c9ba6611f27f6ccc5c6168baa40500c54
Yash Rastogi <rastogiyash2006@gmail.com>
2025-07-11 10:08:38 +0530

    Add files via upload

---
+++ Hello _World.cpp
@@ -0,0 +1,10 @@
+// This is a simple C++ program that prints "Hello, World!" to the console.
+//24070123135
+#include<iostream>
+using namespace std;
+int main() {
+      cout << "Hello  World!" << endl;
+    return 0;
+    
+}
+//Output: Hello  World!
